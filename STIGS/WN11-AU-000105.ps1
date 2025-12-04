776bbf9239329afb0de9eb32afd97bae3a1844d9
lilly-brandon <brandon@brandonlilly.co>
2025-12-04 09:57:28 -0600

    Create WN11-AU-000105.ps1

---
+++ STIGS/WN11-AU-000105.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the system is configured to audit Policy Change – Authentication Policy Change events for successes.
+    
+.NOTES
+    Author          : Brandon Lilly
+    LinkedIn        : https://www.linkedin.com/in/brandon-lilly-92743a392/
+    GitHub          : https://github.com/lilly-brandon
+    Date Created    : 12-04-2025
+    Last Modified   : 12-04-2025
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000105
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN11-AU-000105.ps1 
+#>
+
+auditpol /set /subcategory:"Authentication Policy Change" /success:enable /failure:disable
